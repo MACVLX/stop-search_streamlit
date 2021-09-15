932024c927cd859673f3c93f447380a8602cf66e
MACVLX <miguelvieira21@gmail.com>
2021-09-15 10:45:51 +0100

    improved style

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]
+primaryColor=#A8DBA8
+backgroundColor=#E0E4CC
+secondaryBackgroundColor=#3B8686
+textColor=#262730
+font=sans serif
 " > .streamlit/config.toml
